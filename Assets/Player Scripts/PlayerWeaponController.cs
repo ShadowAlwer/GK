11400f430ef236fd9febb07366ffe00e53a74102
Alex Porebski <porebski.alex@gmail.com>
2018-05-01 16:53:17 +0200

    player is now able to fight back

---
+++ Assets/Player Scripts/PlayerWeaponController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeaponController : MonoBehaviour {
+
+    public int damage;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hit");
+        if (collision.gameObject.tag == "Skeleton")
+        {
+            Debug.Log("Skeleton hit");
+            collision.gameObject.GetComponent<EnemyStatsController>().TakeDamage(damage);
+        }
+
+    }
+}
