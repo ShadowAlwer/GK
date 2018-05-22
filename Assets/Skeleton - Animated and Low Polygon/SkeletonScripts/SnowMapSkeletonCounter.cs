6879eda2c544f254c0f5a1bece53cb378c16bc34
Quamanti <paweszy749@student.polsl.pl>
2018-05-22 19:57:08 +0200

    Added torch, campfire and geysers; Ice walls work now; Little changes on snow terrain (#20)

---
+++ Assets/Skeleton - Animated and Low Polygon/SkeletonScripts/SnowMapSkeletonCounter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowMapSkeletonCounter : MonoBehaviour {
+
+    public static int count = 0;
+
+    // Use this for initialization
+    void Start () {
+        count++;
+    }
+}
---
+++ Assets/SnowTerrain/Scripts/Geyser.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Geyser : MonoBehaviour {
+
+    private bool isForceOn = false;
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("Go", 0f, 5f);
+        InvokeRepeating("Wait", 1f, 5f);
+    }
+
+    void Go()
+    {
+        isForceOn = true;
+        GetComponent<ParticleSystem>().Play();
+    }
+
+    void Wait()
+    {
+        isForceOn = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && isForceOn)
+        {
+            other.GetComponent<Rigidbody>().AddForce(Vector3.up * 1300, ForceMode.Impulse);
+            isForceOn = false;
+        }
+    }
+}
