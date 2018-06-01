d1651b349cbcb5d6c11431142f558b012caf25ad
Quamanti <paweszy749@student.polsl.pl>
2018-06-01 21:06:35 +0200

    Added cutscenes

---
+++ Assets/Cutscenes/StartScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScript : MonoBehaviour {
+
+    public GameObject startCamera;
+    public GameObject mainCamera;
+    public GameObject player;
+
+    private Texture2D blackTex;
+    private float alpha = 1;
+    private bool fadingIn = false;
+    private bool fadingOut = false;
+    private bool scaling = false;
+    private GameObject stats;
+
+    void Start () {
+        stats = GameObject.Find("CanvasScaled");
+        blackTex = new Texture2D(1, 1);
+        blackTex.SetPixel(0, 0, new Color(0, 0, 0, 1));
+        blackTex.Apply();
+
+        player.SetActive(false);
+        player.gameObject.GetComponent<Player_motion>().activeMove = false;
+        player.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+
+        stats.gameObject.GetComponent<Canvas>().enabled = false;
+
+        StartCoroutine(TheSequence());
+    }
+
+    IEnumerator TheSequence() {
+        yield return new WaitForSeconds(2);
+        fadingIn = true;
+
+        yield return new WaitForSeconds(1);
+        gameObject.GetComponent<Animation>().Play();
+
+        yield return new WaitForSeconds(1);
+        player.SetActive(true);
+        scaling = true;
+
+        yield return new WaitForSeconds(8);
+        fadingIn = false;
+        fadingOut = true;
+        yield return new WaitForSeconds(2);
+        mainCamera.SetActive(true);
+        startCamera.SetActive(false);
+        player.gameObject.GetComponent<Player_motion>().activeMove = true;
+        stats.gameObject.GetComponent<Canvas>().enabled = true;
+    }
+
+    void OnGUI() {
+        GUI.color = new Color(0, 0, 0, alpha);
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), blackTex);
+    }
+
+    void Update () {
+        if (fadingIn) {
+            alpha = Mathf.Max(alpha - Mathf.Clamp01(Time.deltaTime / 2), 0);
+        }
+
+        if (fadingOut) {
+            alpha = Mathf.Min(alpha + Mathf.Clamp01(Time.deltaTime / 2), 1);
+        }
+
+        if (scaling) {
+            if (player.transform.localScale.x <= 1) {
+                player.transform.localScale += new Vector3(Time.deltaTime, Time.deltaTime, Time.deltaTime);
+            }
+        }
+    }
+}
--- Assets/Player Scripts/Player_motion.cs
+++ Assets/Player Scripts/Player_motion.cs
@@ -19,7 +19,13 @@ public class Player_motion : MonoBehaviour {
     private float jumpdist=3.0f;
     private float verticalVelocity=0;
     private float distToGround;
-    
+
+    public bool activeMove { get; set; }
+
+    void Start() {
+        activeMove = false;
+    }
+
     void Awake () {
         anim = GetComponent <Animator> ();
         rb=GetComponent<Rigidbody>();
@@ -28,97 +34,86 @@ public class Player_motion : MonoBehaviour {
         //controller=GetComponent<CharacterController>();
     }
     void Update () {
-		forward = Input.GetKey (KeyCode.W);
-        back = Input.GetKey (KeyCode.S);
-        left = Input.GetKey (KeyCode.A);
-        right = Input.GetKey (KeyCode.D);
-		angle_to_rotete=0;
-        float jT=Mathf.Abs(anim.GetFloat("JumpingTiming")-0.60f);
-        Vector3 vector;
-         RaycastHit hit;
+            forward = Input.GetKey(KeyCode.W);
+            back = Input.GetKey(KeyCode.S);
+            left = Input.GetKey(KeyCode.A);
+            right = Input.GetKey(KeyCode.D);
 
-        if (Physics.Raycast(transform.position, -Vector3.up, out hit, 0.5f))
-        {
-           // Debug.Log("FFALSE " + hit.distance);
-            anim.SetBool ("inAir",false);
-        }
-        else
-        {
-           // Debug.Log("TRUE " + hit.distance);
-            anim.SetBool ("inAir",true);
-        }
-        //bool pla=Physics.Raycast(transform.position, -Vector3.up, distToGround, 0.1);
-		
-        int time=(int) (anim.GetFloat("JumpingTiming")*100);
-        if ((anim.GetCurrentAnimatorStateInfo (0).IsTag ("jumpTag") || anim.GetCurrentAnimatorStateInfo (0).IsTag ("jumpStaticTag"))
-            && !anim.GetBool("inTheMiddleOfJumping") )
-        {
+            angle_to_rotete = 0;
+            float jT = Mathf.Abs(anim.GetFloat("JumpingTiming") - 0.60f);
+            Vector3 vector;
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, -Vector3.up, out hit, 0.5f)) {
+                // Debug.Log("FFALSE " + hit.distance);
+                anim.SetBool("inAir", false);
+            } else {
+                // Debug.Log("TRUE " + hit.distance);
+                anim.SetBool("inAir", true);
+            }
+            //bool pla=Physics.Raycast(transform.position, -Vector3.up, distToGround, 0.1);
+
+            int time = (int)(anim.GetFloat("JumpingTiming") * 100);
+        if ((anim.GetCurrentAnimatorStateInfo(0).IsTag("jumpTag") || anim.GetCurrentAnimatorStateInfo(0).IsTag("jumpStaticTag"))
+            && !anim.GetBool("inTheMiddleOfJumping")) {
             //move down
-            vector=new Vector3(0,jT*jumpforce*Time.deltaTime,0);
-            transform.position -=vector;
+            vector = new Vector3(0, jT * jumpforce * Time.deltaTime, 0);
+            transform.position -= vector;
             //move forward
-             vector=new Vector3(jumpdist*Mathf.Sin(kat)*Time.deltaTime,0,(jumpdist)*Mathf.Cos(kat)*Time.deltaTime);
-            transform.position +=vector;
+            vector = new Vector3(jumpdist * Mathf.Sin(kat) * Time.deltaTime, 0, (jumpdist) * Mathf.Cos(kat) * Time.deltaTime);
+            transform.position += vector;
             //move forward
-            if (anim.GetCurrentAnimatorStateInfo (0).IsTag ("jumpStaticTag"))
-            {
+            if (anim.GetCurrentAnimatorStateInfo(0).IsTag("jumpStaticTag")) {
                 Debug.Log("wszedlem");
-                vector=new Vector3(0,20*Time.deltaTime,0);
-                transform.eulerAngles +=vector;
+                vector = new Vector3(0, 20 * Time.deltaTime, 0);
+                transform.eulerAngles += vector;
             }
-            
-            
+
+
         }
-        if ((anim.GetCurrentAnimatorStateInfo (0).IsTag ("jumpTag") || anim.GetCurrentAnimatorStateInfo (0).IsTag ("jumpStaticTag"))
-           && anim.GetBool("inTheMiddleOfJumping") )
-        {
+        if ((anim.GetCurrentAnimatorStateInfo(0).IsTag("jumpTag") || anim.GetCurrentAnimatorStateInfo(0).IsTag("jumpStaticTag"))
+           && anim.GetBool("inTheMiddleOfJumping")) {
             //move up
-            vector=new Vector3(0,jT*jumpforce*Time.deltaTime,0);
-            transform.position +=vector;
+            vector = new Vector3(0, jT * jumpforce * Time.deltaTime, 0);
+            transform.position += vector;
             //move forward
-            vector=new Vector3(jumpdist*Mathf.Sin(kat)*Time.deltaTime,0,(jumpdist)*Mathf.Cos(kat)*Time.deltaTime);
-            transform.position +=vector;
+            vector = new Vector3(jumpdist * Mathf.Sin(kat) * Time.deltaTime, 0, (jumpdist) * Mathf.Cos(kat) * Time.deltaTime);
+            transform.position += vector;
             //move forward
-            if (anim.GetCurrentAnimatorStateInfo (0).IsTag ("jumpStaticTag"))
-            {
+            if (anim.GetCurrentAnimatorStateInfo(0).IsTag("jumpStaticTag")) {
                 Debug.Log("wszedlem");
-                vector=new Vector3(0,20*Time.deltaTime,0);
-                transform.eulerAngles +=vector;
+                vector = new Vector3(0, 20 * Time.deltaTime, 0);
+                transform.eulerAngles += vector;
             }
-            
+
         }
-		
-        anim.SetFloat ("movement",Mathf.Max( Mathf.Abs(Input.GetAxis ("Vertical")),  Mathf.Abs(Input.GetAxis ("Horizontal"))  ));
-        if (anim.GetCurrentAnimatorStateInfo (0).IsTag ("walkingWeap"))
-        {
-            transform.eulerAngles += new Vector3 (0, Mathf.DeltaAngle (transform.eulerAngles.y, center_point.eulerAngles.y+angle_to_rotete) * Time.deltaTime * rotation_speed, 0);
-            anim.SetFloat ("y",Input.GetAxis ("Vertical"));
-            anim.SetFloat ("x",Input.GetAxis ("Horizontal"));
-        }
-        else
-        {
-            {
-                if (Input.GetAxis ("Jump")>0.0f && !isInAir())
+
+        if (activeMove) {
+            anim.SetFloat("movement", Mathf.Max(Mathf.Abs(Input.GetAxis("Vertical")), Mathf.Abs(Input.GetAxis("Horizontal"))));
+            if (anim.GetCurrentAnimatorStateInfo(0).IsTag("walkingWeap")) {
+                transform.eulerAngles += new Vector3(0, Mathf.DeltaAngle(transform.eulerAngles.y, center_point.eulerAngles.y + angle_to_rotete) * Time.deltaTime * rotation_speed, 0);
+                anim.SetFloat("y", Input.GetAxis("Vertical"));
+                anim.SetFloat("x", Input.GetAxis("Horizontal"));
+            } else {
                 {
-                    
-                    anim.SetBool ("jumping",true);
-                    if (anim.GetCurrentAnimatorStateInfo (0).IsTag ("jumpTag"))
-                        kat=transform.eulerAngles.y-10;
-                    else
-                        kat=transform.eulerAngles.y;
-                    kat*=2.0f*3.14f/360.0f;
+                    if (Input.GetAxis("Jump") > 0.0f && !isInAir()) {
+
+                        anim.SetBool("jumping", true);
+                        if (anim.GetCurrentAnimatorStateInfo(0).IsTag("jumpTag"))
+                            kat = transform.eulerAngles.y - 10;
+                        else
+                            kat = transform.eulerAngles.y;
+                        kat *= 2.0f * 3.14f / 360.0f;
+                    }
+                }
+
+                anim.SetFloat("movement", Mathf.Max(Mathf.Abs(Input.GetAxis("Vertical")), Mathf.Abs(Input.GetAxis("Horizontal"))));
+                if (anim.GetCurrentAnimatorStateInfo(0).IsTag("walking")) {
+                    calculate_angle();
+                    transform.eulerAngles += new Vector3(0, Mathf.DeltaAngle(transform.eulerAngles.y, center_point.eulerAngles.y + angle_to_rotete) * Time.deltaTime * rotation_speed, 0);
                 }
             }
-
-            anim.SetFloat ("movement",Mathf.Max( Mathf.Abs(Input.GetAxis ("Vertical")),  Mathf.Abs(Input.GetAxis ("Horizontal"))  ));
-            if (anim.GetCurrentAnimatorStateInfo (0).IsTag ("walking"))
-            {
-                calculate_angle ();
-                transform.eulerAngles += new Vector3 (0, Mathf.DeltaAngle (transform.eulerAngles.y, center_point.eulerAngles.y+angle_to_rotete) * Time.deltaTime * rotation_speed, 0);
-            }
         }
-        
-		
     }
 	void calculate_angle () {
         if (forward && !back) {
@@ -128,16 +123,14 @@ public class Player_motion : MonoBehaviour {
                 angle_to_rotete = 45;
             else
                 angle_to_rotete = 0;
-        } 
-        else if (!forward && back) {
+        } else if (!forward && back) {
             if (left && !right)
                 angle_to_rotete = -135;
             else if (!left && right)
                 angle_to_rotete = 135;
             else
                 angle_to_rotete = 180;
-        } 
-        else {
+        } else {
             if (left && !right)
                 angle_to_rotete = -90;
             else if (right && !left)
