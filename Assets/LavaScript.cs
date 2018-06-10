c65e47637924474109acbc8b6590a5c4204a3478
Quamanti <paweszy749@student.polsl.pl>
2018-06-10 17:46:41 +0200

    Added lava on Dark Terrain

---
+++ Assets/LavaScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaScript : MonoBehaviour {
+
+    private float damageInterval;
+
+    private void OnTriggerEnter(Collider collider) {
+        if (collider.gameObject.tag == "Player") {
+            collider.gameObject.GetComponent<PlayerStats>().TakeDamage(1);
+            damageInterval = Time.time;
+        }
+    }
+
+    private void OnTriggerStay(Collider collider) {
+        if (collider.gameObject.tag == "Player") {
+            if (damageInterval <= Time.time - 1) {
+                collider.gameObject.GetComponent<PlayerStats>().TakeDamage(1);
+                damageInterval = Time.time;
+            }
+        }
+    }
+}
