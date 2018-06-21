9a1e7c63a1d499ac22e8f445bcad2c884a620d12
Alex Porebski <porebski.alex@gmail.com>
2018-06-21 17:37:58 +0200

    add comment to skeleton scripts

--- Assets/Skeleton - Animated and Low Polygon/SkeletonScripts/SkeletonAttackController.cs
+++ Assets/Skeleton - Animated and Low Polygon/SkeletonScripts/SkeletonAttackController.cs
@@ -2,21 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/**
+ * Controls behaviour of skeletons attacks
+ */
 public class SkeletonAttackController : MonoBehaviour {
 
+    /**
+     * Amount of damage that attack make
+     */
     public int damage;
+
+    /**
+     * Determines if object is projectile (spell)
+     */
     public bool isProjectile;
     
 
 	// Use this for initialization
 	void Start () {	}
 	
-	// Update is called once per frame
+    /**
+	* Update is called once per frame and adds force to object if it has Rigidbody component (is spell)
+    */
 	void Update () {
         gameObject.GetComponent<Rigidbody>().AddRelativeForce(Vector3.right * 50);      
     }
 
-
+    /*
+     * Resolves collisions
+     * If collision is with Player deals damage to it
+     * If spell hits something that isn't skeleton spell will be destroyed
+     */
     private void OnCollisionEnter(Collision collision)
     {
        
